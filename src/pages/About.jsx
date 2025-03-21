fa37a8abf70062af5dbfe602c98a7d9ba8a1930f
Varshitha80 <varshithavemula148@gmail.com>
2025-03-21 10:44:05 +0530

    about commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -84,7 +84,7 @@ export default function About() {
                   Let's Collaborate
                 </a>
                 <a 
-                  href="https://drive.google.com/file/d/1WTYWj6j1cTxu9yZZCDUutzE2uXAf676x/view?usp=sharing" 
+                  href="https://drive.google.com/file/d/11xPPfAzWLIq5s8G9aZvN0FlBFoKmwRQh/view?usp=sharing"
                   target='_blank'
                   rel='noopener noreferrer'
                   className="flex items-center gap-2 border border-purple-500 text-purple-300 hover:bg-purple-900/20 px-6 py-3 rounded-lg transition-all"
